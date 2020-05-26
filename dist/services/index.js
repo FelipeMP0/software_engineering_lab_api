b7411ff6856d921471671934a20f97ba196f6c47
Felipe Pena <felipe.m.pena@hotmail.com>
2020-05-25 21:07:39 -0300

    UPT gitignore

--- dist/services/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const routes_1 = require("./test/routes");
-exports.default = [routes_1.TestRouter];
-//# sourceMappingURL=index.js.map
--- dist/src/services/test/TestController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.saveTest = void 0;
-exports.saveTest = () => {
-    return { id: 1, name: 'sdsd' };
-};
-//# sourceMappingURL=TestController.js.map
